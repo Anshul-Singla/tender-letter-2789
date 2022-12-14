42ce28b40554ec025cc69c968b14112f96c307fe
Anirban <anirbansharma36@gmail.com>
2022-12-14 23:28:38 +0530

    added new files

--- frontend/src/components/projects/projects.jsx
+++ frontend/src/components/projects/projects.jsx
@@ -26,7 +26,7 @@ export const Projects = () => {
       .then((res) => setData(res.data))
       .catch((err) => console.log(err.message));
   };
-  
+
   //:::::::::: GETTING DATA ON UI :::::::::::::
 
   useEffect(() => {
@@ -57,8 +57,8 @@ export const Projects = () => {
   };
   //::::::::::::::: UPDATE DATA ::::::::::::::::
 
-  const handleUpdate = async (id) => {
-    let update = prompt("Enter New Description Here");
+  const handleUpdate = async (id, ti) => {
+    let update = prompt(`Enter New Description For Task ID : ${ti}`);
     console.log(update);
     try {
       await axios
@@ -143,20 +143,42 @@ export const Projects = () => {
             </Box>
             {/* -------------API COMPONENT------------- */}
             <Box h={"400px"}>
+              <Box
+                display={"grid"}
+                gridTemplateColumns="repeat(4, 1fr)"
+                w="97%"
+                m="auto"
+                textAlign={"center"}
+                mt="1"
+              >
+                <Box borderRight={"1px solid grey"}>
+                  <Text fontSize="xs">TASK</Text>
+                </Box>
+                <Box borderRight={"1px solid grey"}>
+                  <Text fontSize="xs">ID</Text>
+                </Box>
+                <Box borderRight={"1px solid grey"}>
+                  <Text fontSize="xs">STATUS</Text>
+                </Box>
+                <Box>
+                  <Text fontSize="xs">E/D</Text>
+                </Box>
+              </Box>
+              <hr />
               {data.map((el) => (
                 <Box
                   key={el._id}
                   display={"grid"}
-                  gridTemplateColumns="repeat(3, 1fr)"
-                  w="90%"
+                  gridTemplateColumns="repeat(4, 1fr)"
+                  w="99%"
                   m="auto"
                   mt="2"
                   p="2"
                   border={"1px solid gray"}
-                  borderRadius={"16px"}
+                  borderRadius={"10px"}
                 >
                   <Box display={"grid"} alignItems={"center"}>
-                    <Text as="b">
+                    <Text fontSize="md" as="b">
                       <Checkbox
                         onChange={() => toogle(el._id)}
                         isInvalid
@@ -165,7 +187,15 @@ export const Projects = () => {
                       {el.task.toUpperCase()}
                     </Text>
 
-                    <Text fontSize="sm">NOTE : {el.desc} !</Text>
+                    <Text fontSize="sm">{el.desc}!</Text>
+                  </Box>
+                  <Box
+                    display={"grid"}
+                    textAlign="center"
+                    alignItems={"center"}
+                  >
+                    {" "}
+                    <Text fontSize="sm">{el.taskid}</Text>
                   </Box>
                   <Box
                     display={"grid"}
@@ -178,14 +208,14 @@ export const Projects = () => {
                       </Text>
                     ) : (
                       <Text as="b" color={"green"} fontSize="sm">
-                        COMPLETE AT {new Date().toLocaleTimeString()}
+                        COMPLETED AT {new Date().toLocaleTimeString()}
                       </Text>
                     )}
                   </Box>
-                  <Box textAlign={"right"}>
+                  <Box display="flex" flexWrap={"wrap"}>
                     <Button
-                      onClick={() => handleUpdate(el._id)}
-                      mr="2"
+                      onClick={() => handleUpdate(el._id, el.taskid)}
+                      ml={["8", "8",'5', "20"]}
                       w="10%"
                       border={"1px solid white"}
                     >
@@ -193,6 +223,7 @@ export const Projects = () => {
                     </Button>
                     <Button
                       onClick={() => handleDelete(el._id, el.taskid)}
+                      ml={["8", "8", "2"]}
                       w="10%"
                       border={"1px solid white"}
                     >
